50a7bda78a5acdeaa2427336d99c4841493926dd
Sulav Maharjan <sulavmaharjan63@gmail.com>
2022-07-09 23:50:26 +0545

    Add methods for creating headers and making request

--- hamrobazaar/aioclient.py
+++ hamrobazaar/aioclient.py
@@ -2,6 +2,7 @@ import contextlib
 import typing
 
 import aiohttp
+from aiohttp.typedefs import JSONEncoder
 
 
 class HamrobazaarClient(contextlib.AbstractAsyncContextManager):
@@ -23,7 +24,45 @@ class HamrobazaarClient(contextlib.AbstractAsyncContextManager):
             await self.session.close()
 
     def _get_session(self) -> aiohttp.ClientSession:
-        """Return session object"""
+        """Return or create a session
+
+        Returns:
+            aiohttp.ClientSession
+        """
         if not self.session:
             self.session = aiohttp.ClientSession()
         return self.session
+
+    def _make_headers(self, **kwargs) -> dict:
+        """Construct headers with api_key and extra kwargs
+
+        Returns:
+            dict: constructed headers
+        """
+
+        headers = {k: str(v) for k, v in kwargs.items()}
+        headers["ApiKey"] = self.api_key
+        return headers
+
+    async def _make_request(
+        self, method: str, url: str, json: typing.Optional[JSONEncoder] = None
+    ) -> dict:
+        """Makes a request to the url
+
+        Args:
+            method (str): HTTP method to use
+            url (str): URL to make request to
+            json (typing.Optional[JSONEncoder], optional): Json body to send during request. Defaults to None.
+
+        Returns:
+            dict: decoded json response
+        """
+        session = self._get_session()
+        headers = self._make_headers()
+
+        async with session.request(method, url, headers=headers, json=json) as response:
+            try:
+                return await response.json()
+            except aiohttp.ClientResponseError as e:
+                # Todo: Log messages
+                response.raise_for_status()
